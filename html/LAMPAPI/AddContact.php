6f1b347b813bc0339a427d91266f791c93e98f31
sarahwhitfi <57811440+sarahwhitfi@users.noreply.github.com>
2023-09-11 20:59:26 -0400

    Update AddContact.php
    Updating Headers

--- html/LAMPAPI/AddContact.php
+++ html/LAMPAPI/AddContact.php
@@ -7,6 +7,11 @@
 	$LastName = $inData["LastName"];
 	$UserID = $inData["UserID"];
 
+	header("Access-Control-Allow-Origin: http://cop4331-f23.com");
+	header("Access-Control-Allow_Methods: POST, OPTIONS");
+	header("Access-Control-Allow_Headers: Content-Type");
+	header("Access-Control-Max-Age: 86400");
+		
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
 	if ($conn->connect_error) 
 	{
