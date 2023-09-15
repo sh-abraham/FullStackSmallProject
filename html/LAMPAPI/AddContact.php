6739808b614a4e20ec9bf645a31f3f9d7fe1c371
sarahwhitfi <57811440+sarahwhitfi@users.noreply.github.com>
2023-09-15 15:03:07 -0400

    Update AddContact.php
    Getting rid of headers because we don't need them anymore.

--- html/LAMPAPI/AddContact.php
+++ html/LAMPAPI/AddContact.php
@@ -9,10 +9,10 @@
 	$Email = $inData["Email"];
 	$Phone = $inData["Phone"];
 
-	header("Access-Control-Allow-Origin: http://cop4331-f23.com");
-	header("Access-Control-Allow_Methods: POST, OPTIONS");
-	header("Access-Control-Allow_Headers: Content-Type");
-	header("Access-Control-Max-Age: 86400");
+	#header("Access-Control-Allow-Origin: http://cop4331-f23.com");
+	#header("Access-Control-Allow_Methods: POST, OPTIONS");
+	#header("Access-Control-Allow_Headers: Content-Type");
+	#header("Access-Control-Max-Age: 86400");
 		
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
 	if ($conn->connect_error) 
