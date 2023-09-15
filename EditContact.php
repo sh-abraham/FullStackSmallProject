42ef1adc5f6e0597d11addb8a79dbccac34e8257
sarahwhitfi <57811440+sarahwhitfi@users.noreply.github.com>
2023-09-15 11:39:03 -0400

    Update EditContact.php
    Added ability to update phone number.

--- EditContact.php
+++ EditContact.php
@@ -5,14 +5,15 @@ $ID = $inData["ID"];
 $firstName = $inData["FirstName"];
 $lastName = $inData["LastName"];
 $email = $inData["Email"];
+$phone = $inData["Phone"];
 
 $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
 if ($conn->connect_error) {
     returnWithError($conn->connect_error);
 } else {
     // Update the Contact information from database.
-    $stmt = $conn->prepare("UPDATE Contacts SET FirstName=?, LastName=?, Email=? WHERE ID = ?");
-    $stmt->bind_param("sssi", $firstName, $lastName, $email, $ID);
+    $stmt = $conn->prepare("UPDATE Contacts SET FirstName=?, LastName=?, Email=?, Phone=? WHERE ID = ?");
+    $stmt->bind_param("ssssi", $firstName, $lastName, $email, $phone, $ID);
     
     if ($stmt->execute()) {
         returnWithError("");
