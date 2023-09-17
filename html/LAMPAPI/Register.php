c7dfdce9c9e509d68040502ef240e6369b26d3da
sarahwhitfi <57811440+sarahwhitfi@users.noreply.github.com>
2023-09-17 10:31:28 -0400

    Update Register.php
    Prevented Users from having duplicate Login values.

--- html/LAMPAPI/Register.php
+++ html/LAMPAPI/Register.php
@@ -1,9 +1,9 @@
 
 <?php
-
 	$inData = getRequestInfo();
 	
 	$ID = 0;
+	$Login = $inData["Login"];
 
 	#header("Access-Control-Allow-Origin: http://cop4331-f23.com");
 	#header("Access-Control-Allow_Methods: POST, OPTIONS");
@@ -11,22 +11,35 @@
 	#header("Access-Control-Max-Age: 86400");
 
     $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	try{
 	if ($conn->connect_error) 
 	{
 		returnWithError( $conn->connect_error );
 	} 
 	else
 	{
-		try{
-		$stmt = $conn->prepare("INSERT into Users (FirstName, LastName, Login, Password) VALUES(?,?,?,?)");
-		$stmt->bind_param("ssss", $inData["FirstName"], $inData["LastName"], $inData["Login"], $inData["Password"]);
+		$sql = "SELECT * FROM Users WHERE Login=?";
+		$stmt = $conn->prepare($sql);
+		$stmt->bind_param("s", $Login);
 		$stmt->execute();
-		$stmt->close();
-		$conn->close();
-		returnWithError("");
-		}catch (Exception $e) {
-			returnWithError("An error occurred: " . $e->getMessage());
+		$result = $stmt->get_result();
+		$nrows = mysqli_num_rows($result);
+		if ($nrows == 0)
+		{
+			$stmt = $conn->prepare("INSERT into Users (FirstName, LastName, Login, Password) VALUES(?,?,?,?)");
+			$stmt->bind_param("ssss", $inData["FirstName"], $inData["LastName"], $Login, $inData["Password"]);
+			$stmt->execute();
+			$stmt->close();
+			$conn->close();
+			returnWithError("");
 		}
+		else
+		{
+			returnWithError("Login taken. Try another!");
+		}
+	}
+	}catch (Exception $e) {
+		returnWithError("An error occurred: " . $e->getMessage());
 	}
 
 	function getRequestInfo()
@@ -45,5 +58,5 @@
 		$retValue = '{"error":"' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
 	}
-	
+
 ?>
